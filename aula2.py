ea13625c495b00fc4a0c01dac0c89e0b55b4972f
Rodolfo <105673932+rodocs777@users.noreply.github.com>
2024-03-18 21:25:08 -0300

    Create aula2.py

---
+++ aula2.py
@@ -0,0 +1,10 @@
+print(12, 34, sep =" ",  end = '\n') #\n quebra linha
+print(56, 76, sep=',')  # separa tudo por ,
+
+
+
+
+
+
+# para comentar variaas linhas, ctrl k solta e ctrol c
+# para descomentar contral k e control u
